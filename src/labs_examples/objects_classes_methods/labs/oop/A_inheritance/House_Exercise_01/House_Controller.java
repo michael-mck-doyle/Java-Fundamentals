a9f8ec27cc438d5f301844225bc90ff610e4f9f9
Michael Doyle <meyekkul@gmail.com>
2019-03-27 21:20:16 +0800

    oop labs update

---
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/House_Exercise_01/House_Controller.java
@@ -0,0 +1,27 @@
+package labs_examples.objects_classes_methods.labs.oop.A_inheritance.House_Exercise_01;
+
+public class House_Controller {
+
+
+    public static void main(String[] args) {
+
+        House newHouse  = new House(11, "Emerald Avenue");
+
+        System.out.println(newHouse);
+
+        System.out.println((newHouse.getHouseNumber()) + " " + newHouse.getStreetName());
+
+        newHouse.setHouseNumber(15);
+        newHouse.setStreetName("Ruby Road");
+
+        System.out.println((newHouse.getHouseNumber()) + " " + newHouse.getStreetName());
+
+        Blinds venetian = new Blinds(18, "Diamond", 3, 2, "Rectangle", "green", "horizontal", "laminated veneer",
+                180, 250 );
+
+
+
+        System.out.println(venetian);
+
+    }
+}
