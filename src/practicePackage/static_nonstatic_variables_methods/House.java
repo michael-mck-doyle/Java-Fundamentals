5da21912a5b3baa9546183970ea8b508fcf206bf
Michael Doyle <meyekkul@gmail.com>
2019-04-30 12:00:24 +0800

    SQL practice

--- src/practicePackage/static_nonstatic_variables_methods/House.java
+++ src/practicePackage/static_nonstatic_variables_methods/House.java
@@ -1,4 +1,25 @@
 package practicePackage.static_nonstatic_variables_methods;
 
 public class House {
+
+
+    int squareFeet;
+    String address;
+    static int numHousesBuilt;
+
+    public House(int squareFeet, String address) {
+
+        this.squareFeet = squareFeet;
+        this.address = address;
+        numHousesBuilt++;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "squareFeet=" + squareFeet +
+                ", address='" + address + '\'' +
+                ", numHousesBuilt = " +  numHousesBuilt + '}';
+                
+    }
 }
