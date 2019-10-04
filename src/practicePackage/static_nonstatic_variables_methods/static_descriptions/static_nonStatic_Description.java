1ab61009f172000060d85ac3fc40597145d53329
Michael Doyle <meyekkul@gmail.com>
2019-10-04 11:38:11 +0800

    updating static practice files

---
+++ src/practicePackage/static_nonstatic_variables_methods/static_descriptions/static_nonStatic_Description.java
@@ -0,0 +1,27 @@
+
+
+
+/*
+Static Variable
+- doesn't need an object to be created for it to exist, it can already be accessed
+- any objects created for the Class with the static variable will all point to the same static variable
+- objects will not get their own copy of a static variable; they all refer to the same static variable
+- static variables are declared when they should be used independently of any object
+- does each object need to get its own copy of a variable?  This is the main consideration when deciding if a variable should be static
+
+Static Method
+- doesn't need an object to be created for it to exist
+- static members are declared when they should be used independently of any object
+- does each object need its own copy of a method? This is the main consideration when deciding if a method should be static
+
+
+General
+- static variables and methods belong to their Class
+
+Calling a static variable or method takes the following format:
+
+Classname.variable or method name
+
+
+
+/*
