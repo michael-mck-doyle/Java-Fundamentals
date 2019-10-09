4dac861a33e70d9e63415e2683458b2898103784
Michael Doyle <meyekkul@gmail.com>
2019-10-09 08:15:29 +0800

    updating static lab exercises

---
+++ src/labs_examples/static_nonstatic/labs/NonStatic_Static_SameClass.java
@@ -0,0 +1,4 @@
+package labs_examples.static_nonstatic.labs;
+
+public class NonStatic_Static_SameClass {
+}
---
+++ src/labs_examples/static_nonstatic/labs/Static_NonStatic_SameClass.java
@@ -0,0 +1,4 @@
+package labs_examples.static_nonstatic.labs;
+
+public class Static_NonStatic_SameClass {
+}
