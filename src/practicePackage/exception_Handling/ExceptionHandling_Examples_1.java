2a312b3680f80a0b3de94f3520cdf0436bf1f686
Michael Doyle <meyekkul@gmail.com>
2019-10-21 15:25:39 +0800

    Updating Exception Handling examples

---
+++ src/practicePackage/exception_Handling/ExceptionHandling_Examples_1.java
@@ -0,0 +1,13 @@
+package practicePackage.exception_Handling;
+
+public class ExceptionHandling_Examples_1 {
+
+    public static void main(String[] args) {
+
+        int[] nums = { 23, 12, 18};
+
+        System.out.println(nums[3]);
+
+        System.out.println("Program Complete");
+    }
+}
