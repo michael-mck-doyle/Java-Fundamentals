2c7de60baf2562afd25f858750c94acd8024f0a2
Michael Doyle <meyekkul@gmail.com>
2019-10-21 16:13:18 +0800

    Updating Exception Handling examples

--- src/practicePackage/exception_Handling/ExceptionHandling_Examples_1.java
+++ src/practicePackage/exception_Handling/ExceptionHandling_Examples_1.java
@@ -6,7 +6,14 @@ public class ExceptionHandling_Examples_1 {
 
         int[] nums = { 23, 12, 18};
 
-        System.out.println(nums[3]);
+        try {
+            System.out.println(nums[3]);
+
+        } catch (ArrayIndexOutOfBoundsException exc1){
+            System.out.println("Error detected");
+            exc1.printStackTrace();
+        }
+
 
         System.out.println("Program Complete");
     }
