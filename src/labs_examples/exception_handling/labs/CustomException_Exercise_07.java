a01c4b7af0fe7b0c5a428300e8a128b9d03a33df
Michael Doyle <meyekkul@gmail.com>
2019-11-03 19:11:40 +0800

    updating exception handling practice folder

--- src/labs_examples/exception_handling/labs/CustomException_Exercise_07.java
+++ src/labs_examples/exception_handling/labs/CustomException_Exercise_07.java
@@ -6,3 +6,34 @@ package labs_examples.exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+
+
+
+
+public class CustomException_Exercise_07 {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        try {
+
+            System.out.println(nums[5]);
+
+        } catch (ArrayIndexOutOfBoundsException exc) {
+
+            System.out.println(exc.toString());
+        }
+
+        class JavaPractice extends Exception {
+
+            @Override
+            public String toString() {
+                return "You need more Java practice!";
+            }
+        }
+    }
+}
+
+
