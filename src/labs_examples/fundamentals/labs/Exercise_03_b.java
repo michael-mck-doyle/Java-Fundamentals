c065bcb369f3460cbc98321180da5cd9bdf79452
Michael Doyle <meyekkul@gmail.com>
2020-01-26 19:07:16 +0800

    updating Fundamentals labs

---
+++ src/labs_examples/fundamentals/labs/Exercise_03_b.java
@@ -0,0 +1,11 @@
+package labs_examples.fundamentals.labs;
+
+public class Exercise_03_b {
+
+    public static void main(String[] args) {
+
+        System.out.println("Re-doing Exercise 3");
+        System.out.println("Practice makes perfect!");
+    }
+
+}
