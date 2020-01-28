d465fb696ce0ee2e4c6fe4414c58b4433882e6ae
Michael Doyle <meyekkul@gmail.com>
2020-01-28 20:47:02 +0800

    updating data types labs

---
+++ src/labs_examples/datatypes_operators/labs/Exercise_3a.java
@@ -0,0 +1,45 @@
+package labs_examples.datatypes_operators.labs;
+
+public class Exercise_3a {
+
+    /**
+     * Please demonstrate the use of all arithmetic operators below. These include:
+     *
+     * addition, subtraction, multiplication, division and modulus
+     *
+     */
+
+
+        public static void main(String[] args) {
+
+            char g = 'a';
+            char h = 'b';
+
+
+
+            char a = 5 + 7;
+            System.out.println("The value of 'a' is " +a);
+
+            double b = 17.89 - 7.63;
+            System.out.println("The value of 'b' is " + b);
+
+            float c = (float) (3.81 * 4.3);
+            System.out.println("The value of 'c' is " + c);
+
+            long d = 7876543654321l / 2;
+            System.out.println("The value of 'd' is " +d);
+
+            int e = 19 % 6;
+            System.out.println("The value of 'e' is " +e);
+
+            // write your code below
+
+            int f =  (g + h);
+            System.out.println(f);
+
+            char k = a;
+            System.out.println("The value of i is" + k);
+        }
+
+    }
+
