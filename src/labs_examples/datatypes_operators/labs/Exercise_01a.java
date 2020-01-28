8403dfb2c1af7c43e0cbe064db15bcf23568acc6
Michael Doyle <meyekkul@gmail.com>
2020-01-28 20:01:18 +0800

    updating data types labs

---
+++ src/labs_examples/datatypes_operators/labs/Exercise_01a.java
@@ -0,0 +1,28 @@
+package labs_examples.datatypes_operators.labs;
+
+public class Exercise_01a {
+
+
+/**
+ * Data Types and Operators Exercise 1: Variable declaration and Initialization
+ *
+ *      Write the necessary code to complete the following:
+ *
+ *      1. Declare an int, a double, a float and a char
+ *      2. Assign appropriate values to each
+ *      3. Print out each variable to the console
+ *
+ *
+ */
+
+public static void main(String[] args) {
+
+
+    int Int1 = 12;
+    Double Doub1 = 123.321;
+    Float Flo1 = 37.70f;
+
+    System.out.println(Int1 + " " + Doub1 + " " + Flo1);
+}
+
+}
