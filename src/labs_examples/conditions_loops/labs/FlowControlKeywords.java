b6d673ae5642f96b949dab2064f5a493e4d0847e
Michael Doyle <meyekkul@gmail.com>
2020-02-10 20:22:53 +0800

    updating flow control keyword labs

--- src/labs_examples/conditions_loops/labs/FlowControlKeywords.java
+++ src/labs_examples/conditions_loops/labs/FlowControlKeywords.java
@@ -4,19 +4,61 @@ public class FlowControlKeywords {
 
     public static void main(String[] args) {
 
-//Continue keyword
+        //Continue keyword
 
         for (int x = 0; x < 100; x += 1) {
 
-                if (x % 3 != 0) {
+            if (x % 3 != 0) {
 
-                    continue;
-
-                } System.out.println(x);
-        }
-
+                continue;
 
             }
+            System.out.println("x = " + x);
         }
+
+        System.out.println();
+
+        //Break keyword
+
+        for (int y = 0; y < 100; y += 1) {
+
+            if (y == 8) {
+
+                break;
+
+            }
+
+            System.out.println("y = " + y);
+
+        }
+
+        int c = someMethod(18);
+        System.out.println(c);
+
+    }
+    // Return keyword
+
+
+    public static int someMethod(int a) {
+
+        int b = a % 2;
+
+        if (b != 0) {
+            System.out.println(" 'a' is an odd number");
+            return b;
+        }
+        else if (b == 0) {
+            System.out.println("'a' is an even number");
+            return a;
+
+        } else {
+            System.out.println(" 'a' is from another dimention" +
+                    "");
+            return a;
+        }
+
+    }
+}
+
 
 
