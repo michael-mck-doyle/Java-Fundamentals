f3acd32b7d542df3f906b2a9e898ca319bed8e80
Michael Doyle <meyekkul@gmail.com>
2020-02-10 19:56:02 +0800

    updating flow control keyword labs

---
+++ src/labs_examples/conditions_loops/labs/FlowControlKeywords.java
@@ -0,0 +1,22 @@
+package labs_examples.conditions_loops.labs;
+
+public class FlowControlKeywords {
+
+    public static void main(String[] args) {
+
+//Continue keyword
+
+        for (int x = 0; x < 100; x += 1) {
+
+                if (x % 3 != 0) {
+
+                    continue;
+
+                } System.out.println(x);
+        }
+
+
+            }
+        }
+
+
