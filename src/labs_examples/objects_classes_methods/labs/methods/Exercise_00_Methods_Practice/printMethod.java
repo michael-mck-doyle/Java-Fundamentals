8f77135ae69781a6036d5d44800960316d6a182e
Michael Doyle <meyekkul@gmail.com>
2020-02-25 12:26:23 +0800

    updating method labs

---
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_00_Methods_Practice/printMethod.java
@@ -0,0 +1,17 @@
+package labs_examples.objects_classes_methods.labs.methods.Exercise_00_Methods_Practice;
+
+public class printMethod {
+
+    public static void main(String[] args) {
+
+        printMethod();
+
+    }
+
+    public static void printMethod() {
+
+        System.out.println("printing printMethod");
+    }
+
+
+}
